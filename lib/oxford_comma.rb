a88de9740b634f064f07fbd77def88cede6db39e
Michelle_Mungai <chellymungai@gmail.com>
2023-02-10 10:25:29 +0300

    Completed assign,emt

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    case 
+    when array.length == 1 then array.join(" , ")
+    when array.length == 2 then array.join(" and ")
+    when array.length > 2 then array.slice(0,array.length-1).join(", ") + ", and " + array.slice(array.length-1,array.length-1).join
+end
 end
